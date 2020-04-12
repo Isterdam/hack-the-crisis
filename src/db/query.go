8c2033660be0a34d41509475a01f7ceba30ad1e5
miwink <michael.w.99@hotmail.com>
2020-04-12 22:09:59 +0200

    Small fix

--- src/db/query.go
+++ src/db/query.go
@@ -13,7 +13,7 @@ var queries = []KVP{
 	KVP{K: "book/remove", V: "DELETE FROM bookings WHERE code=$1"},
 	KVP{K: "booking/get", V: "SELECT * FROM bookings"},
 	KVP{K: "booking/getBySlotID", V: "SELECT * FROM bookings WHERE slot_id=$1"},
-	KVP{K: "company/insert", V: "INSERT INTO company (id, name, adress, city, country, post_code, contact_firstname, contact_number, contact_lastname, verified, email, password, lon, lat) VALUES (DEFAULT, $1, $2, $3, $4, $5, $6, $7, $8, DEFAULT, $9, $10, $11, $12)"},
+	KVP{K: "company/insert", V: "INSERT INTO company (id, name, adress, city, country, post_code, contact_firstname, contact_number, contact_lastname, verified, email, password, lon, lat, contact_email) VALUES (DEFAULT, $1, $2, $3, $4, $5, $6, $7, $8, DEFAULT, $9, $10, $11, $12, $13)"},
 	KVP{K: "company/update/location", V: "UPDATE company SET name=$2, adress=$3, city=$4, country=$5, post_code=$6, lon=$7, lat=$8 WHERE id=$1 RETURNING *"},
 	KVP{K: "company/update/contact", V: " UPDATE company SET contact_firstname=$2, contact_lastname=$3, contact_number=$4 WHERE id=$1 RETURNING *"},
 	KVP{K: "company/update/password", V: "UPDATE company SET password=$2 WHERE id=$1 RETURNING *"},
