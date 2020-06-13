80e88910571b65d07412e4254fdb439f15faaca6
Alexander Brokking <brokki@kth.se>
2020-06-13 18:41:44 +0200

    Fixed email sending

--- src/api/mailSender.go
+++ src/api/mailSender.go
@@ -7,16 +7,14 @@ import (
 )
 
 func SendMail(to, subject, content string) {
-	from := "shopalone.se@gmail.com"
+	from := "team@shopalone.se"
 	password := os.Getenv("MAILPASS")
-	msg := "From: " + from + "\n" +
+	msg := "From: Team ShopAlone <" + from + "> \n" +
 		"To: " + to + "\n" +
 		"Subject:" + subject + "\n" +
 		content
 
-	err := smtp.SendMail("smtp.gmail.com:587",
-		smtp.PlainAuth("", from, password, "smtp.gmail.com"),
-		from, []string{to}, []byte(msg))
+	err := smtp.SendMail("smtp.gmail.com:587", smtp.PlainAuth("", from, password, "smtp.gmail.com"), from, []string{to}, []byte(msg))
 
 	if err != nil {
 		fmt.Println(err)
