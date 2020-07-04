f98f53535a2adc5e849877db768a0e3bed198c63
unknown <adrianmu@kth.se>
2020-07-04 17:51:14 +0200

    yeet

--- src/api/public.go
+++ src/api/public.go
@@ -112,11 +112,9 @@ func BookTime(c *gin.Context) {
 	timeStart := timeSlot.StartTime.Time.In(loc)
 	timeStop := timeSlot.EndTime.Time.In(loc)
 
-	confirmation := "Hej " + booking.FirstName.String + "!\n\n" + "Vänligen bekräfta din bokning på " + store.Name.String + " den " + timeStart.Format("2/1") + " klockan " + timeStart.Format("15:04") + "-" + timeStop.Format("15:04") + " i länken nedan:\n\n" + url
+	confirmation := "Hej " + booking.FirstName.String + "!\n\n" + "Vänligen bekräfta din bokning på " + store.Name.String + " den " + timeStart.Format("2/1") + " klockan " + timeStart.Format("15:04") + "-" + timeStop.Format("15:04") + " i länken nedan:\n\n" + url + "\n\nNotera att din bokning först blir giltig när du bekräftat den genom länken ovan"
 
-	fmt.Println(confirmation)
-
-	// go Send_text(c, booking.PhoneNumber.String, confirmation)
+	go Send_text(c, booking.PhoneNumber.String, confirmation)
 
 	c.JSON(200, gin.H{
 		"message": "Booking was successful",
