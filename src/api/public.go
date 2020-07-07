fa6662699b52fbf0d5d70800097e2d50bf9b4153
unknown <adrianmu@kth.se>
2020-07-07 14:36:24 +0200

    fixed return statements in errors

--- src/api/public.go
+++ src/api/public.go
@@ -106,12 +106,14 @@ func BookTime(c *gin.Context) {
 		c.JSON(http.StatusInternalServerError, gin.H{
 			"message": "Could not get slot!",
 		})
+		return
 	}
 	store, err := db.GetCompanyByID(dbbb, int(timeSlot.CompanyID.Int64))
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{
 			"message": "Could not get company by ID!",
 		})
+		return
 	}
 
 	// only gets the zeroth element of zone list (because European countries only have single time zones)
@@ -120,6 +122,7 @@ func BookTime(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, gin.H{
 			"message": "Could not find the location for time zone!",
 		})
+		return
 	}
 	timeStart := timeSlot.StartTime.Time.In(loc)
 	timeStop := timeSlot.EndTime.Time.In(loc)
@@ -144,6 +147,7 @@ func hasAlreadyBooked(phoneNum string, dbb *db.DB, c *gin.Context) bool {
 		c.JSON(http.StatusInternalServerError, gin.H{
 			"message": "Something went wrong when trying to check for previous bookings with this phone number",
 		})
+		return
 	}
 
 	currentTime := time.Now().UTC()
@@ -157,6 +161,7 @@ func hasAlreadyBooked(phoneNum string, dbb *db.DB, c *gin.Context) bool {
 				c.JSON(http.StatusInternalServerError, gin.H{
 					"message": "Could not get slot by ID in checking if phone number has already booked",
 				})
+				return
 			}
 			if currentTime.Before(slot.StartTime.Time) {
 				return true
@@ -293,6 +298,7 @@ func GetSlotLoad(c *gin.Context) {
 		c.JSON(http.StatusInternalServerError, gin.H{
 			"message": "Could not parse slot ID into integer",
 		})
+		return
 	}
 
 	dbb, exist := c.Get("db")
